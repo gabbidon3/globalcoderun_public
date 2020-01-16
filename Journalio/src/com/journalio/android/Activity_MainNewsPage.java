7a92bdbad35231a35cb41de5009f4124e1ad32b4
emap <emap@192.168.1.63>
2020-01-16 23:35:54 +0000

    Journalio first commit :
    This project is been developed together with the coding ninjas to help
    make sure, the truth can be told and told in a way that cannot be
    stopped and those that choose to tell these truthful stories can be
    rewarded in cash or kind. We hope that journalists and reporters would
    make a great living and even better would be for these awesome writters
    to have a far more massive reader base.
    
    Journalio ~ Truth in (a rewarding) light.

---
+++ Journalio/src/com/journalio/android/Activity_MainNewsPage.java
@@ -0,0 +1,17 @@
+package com.journalio.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity_MainNewsPage extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.main_news_page_layout);
+		
+	}
+	
+}
